13bae685c326c00bcb1e32073051f9bd1d7e50d0
tiia90 <62059457+tiia90@users.noreply.github.com>
2022-12-29 22:34:30 +0200

    only added connection to database,no function yet

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = 3000;
 
 app.use(bodyParser.urlencoded ({ extended : true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
+
+//adding connection to database, not using env. now
+const mongoURL = 'mongodb+srv://tiia90:3heivaan4@cluster0.igutn.mongodb.net/moviedb?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, {useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 app.get("/", (req, res) => {
     res.render("index");
