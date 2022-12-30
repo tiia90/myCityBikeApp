6badf365f18c54699811267c4201cadf861d2aac
tiia90 <62059457+tiia90@users.noreply.github.com>
2022-12-30 15:12:39 +0200

    find data from database

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 const port = 3000;
 
 app.use(bodyParser.urlencoded ({ extended : true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
+mongoose.set('strictQuery', false);
+
 //adding connection to database, not using env. now
-const mongoURL = 'mongodb+srv://tiia90:3heivaan4@cluster0.igutn.mongodb.net/moviedb?retryWrites=true&w=majority';
-mongoose.connect(mongoURL, {useNewUrlParser: true , useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error'));
+mongoose.connect('mongodb+srv://tiia90:3heivaan4@cluster0.igutn.mongodb.net/bikeRoutes?retryWrites=true&w=majority');
+
+
+const stationsSchema = {
+    Nimi: String
+}
+const Station = mongoose.model('Station', stationsSchema);
 
 app.get("/", (req, res) => {
-    res.render("index");
+    Station.find({}, function(err, stations) {
+    res.render('index', {
+        stationsList: stations
+    })
+})
 })
 
 app.get("/getroutes", (req, res) => {
